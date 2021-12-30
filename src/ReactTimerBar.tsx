e4c1e4b705e49731ab714456af7c2160ed4493fc
thuoe <thuoeddie17@gmail.com>
2021-12-30 00:13:25 +0000

    refactor: replace static css files with emotion

--- src/ReactTimerBar.tsx
+++ src/ReactTimerBar.tsx
@@ -1,13 +1,36 @@
 import * as React from 'react';
+import styled from '@emotion/styled';
 import './Timer.css';
+
+type TimerBarProps = {
+  fillColor: string;
+  fillWidth: number;
+};
 
 type ReactTimerProps = {
   time: number;
   fillColor: string;
 };
 
+const Container = styled.div(() => ({
+  position: 'relative',
+  backgroundColor: 'red',
+  borderRadius: '3px',
+  width: '300px',
+  height: '100px',
+  textAlign: 'center',
+}));
+
+const Timerbar = styled.div<TimerBarProps>(({ fillWidth, fillColor }) => ({
+  position: 'relative',
+  height: '100%',
+  transition: 'width 200ms',
+  backgroundColor: fillColor,
+  width: `${fillWidth}px`,
+}));
+
 export const ReactTimerBar = ({ time = 0, fillColor = 'blue' }: ReactTimerProps): JSX.Element => {
-  const timerBar = React.useRef<HTMLDivElement | null>(null);
+  const container = React.useRef<HTMLDivElement | null>(null);
   const [timeElapsed, setTimeElapsed] = React.useState<number>(0);
 
   function formatTime(newTime: number): string {
@@ -35,15 +58,15 @@ export const ReactTimerBar = ({ time = 0, fillColor = 'blue' }: ReactTimerProps)
     };
   }, [timeElapsed]);
 
-  const timerWidth = timerBar.current ? timerBar.current.offsetWidth : 0;
+  const timerWidth = container.current ? container.current.offsetWidth : 0;
   const fillWidth = (timeElapsed / time) * timerWidth;
 
   return (
     <>
-      <div ref={timerBar} className="timer-container">
-        <div className="timer-bar" style={{ backgroundColor: fillColor, width: `${fillWidth}px` } }></div>
+      <Container ref={container}>
+        <Timerbar fillColor={fillColor} fillWidth={fillWidth}></Timerbar>
         <span title="Time Elapsed">Time Elapsed {formatTime(time)}</span>
-      </div>
+      </Container>
     </>
   ); 
 };
