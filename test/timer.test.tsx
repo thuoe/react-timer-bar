c4c089a7e4b1fc8d09b044eb285ee82e900f2669
thuoe <thuoeddie17@gmail.com>
2022-01-01 17:13:12 +0000

    test: verify width of bar after a certain period

--- test/timer.test.tsx
+++ test/timer.test.tsx
@@ -1,7 +1,10 @@
 
 import * as React from 'react';
 import { screen, render, act, waitFor } from '@testing-library/react';
+import { matchers } from '@emotion/jest';
 import { ReactTimerBar } from '../src/ReactTimerBar';
+
+expect.extend(matchers);
 
 describe('Timer tests', () => {
   beforeEach(() => {
@@ -30,6 +33,24 @@ describe('Timer tests', () => {
     });
     const span = await screen.findByTitle('Time Elapsed');
     await waitFor(() => expect(span.innerHTML).toBe('Time Elapsed 0:04'));
+  });
+  
+  it('Verify progess bar fill width after a certain period', async () => {
+    const timeinMs = 6000;
+    const advanceTimeinMs = 3000;
+    const mockWidth = 500;
+    const { container } = render(<ReactTimerBar time={timeinMs / 1000} fillColor="blue"/>);
+    act(() => {
+      jest.advanceTimersByTime(advanceTimeinMs);
+    });
+    const mainContainer: HTMLDivElement = container.querySelector('#container');
+    const progressBar: HTMLDivElement = container.querySelector('#progress-bar');
+
+    // jsdom cannot support layouts so measurements will always return 0 unless stubbed;
+    Object.defineProperty(mainContainer, 'offsetWidth', { configurable: true, value: mockWidth });
+
+    const expectedFillWidth = advanceTimeinMs / timeinMs * mockWidth;
+    await waitFor(() => expect(progressBar).toHaveStyleRule('width', `${expectedFillWidth}px`));
   });
 
 });
