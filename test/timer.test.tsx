4d15ed8fb35cf6f179a8e43b865ba524c3b255f9
Eddie Thuo <thuoeddie17@gmail.com>
2022-04-06 17:53:24 +0100

    test: flow directions

--- test/timer.test.tsx
+++ test/timer.test.tsx
@@ -7,6 +7,11 @@ import { ReactTimerBar } from '../src/ReactTimerBar';
 expect.extend(matchers);
 
 describe('Timer tests', () => {
+  const timeinMs = 6000;
+  const advanceTimeinMs = 3000;
+  const mockWidth = 500;
+  const mockHeight = 500;
+
   beforeEach(() => {
     jest.useFakeTimers();
   });
@@ -36,9 +41,6 @@ describe('Timer tests', () => {
   });
   
   it('Verify progess bar fill width after a certain period', async () => {
-    const timeinMs = 6000;
-    const advanceTimeinMs = 3000;
-    const mockWidth = 500;
     const { container } = render(<ReactTimerBar time={timeinMs / 1000} fillColor="blue"/>);
     act(() => {
       jest.advanceTimersByTime(advanceTimeinMs);
@@ -53,4 +55,50 @@ describe('Timer tests', () => {
     await waitFor(() => expect(progressBar).toHaveStyleRule('width', `${expectedFillWidth}px`));
   });
 
+  it('should render with a flow direction from rightToLeft', async () => {
+    const { container } = render(<ReactTimerBar time={timeinMs / 1000} fillColor="blue" direction='rightToLeft'/>);
+    act(() => {
+      jest.advanceTimersByTime(advanceTimeinMs);
+    });
+    const mainContainer: HTMLDivElement = container.querySelector('#container');
+    const progressBar: HTMLDivElement = container.querySelector('#progress-bar');
+
+    // jsdom cannot support layouts so measurements will always return 0 unless stubbed;
+    Object.defineProperty(mainContainer, 'offsetWidth', { configurable: true, value: mockWidth });
+
+    const expectedFillWidth = advanceTimeinMs / timeinMs * mockWidth;
+    await waitFor(() => expect(progressBar).toHaveStyleRule('width', `${expectedFillWidth}px`));
+  });
+
+  it('should render with a flow direction from bottomToTop', async () => {
+    const { container } = render(<ReactTimerBar time={timeinMs / 1000} fillColor="blue" direction='bottomToTop'/>);
+    act(() => {
+      jest.advanceTimersByTime(advanceTimeinMs);
+    });
+    const mainContainer: HTMLDivElement = container.querySelector('#container');
+    const progressBar: HTMLDivElement = container.querySelector('#progress-bar');
+
+    // jsdom cannot support layouts so measurements will always return 0 unless stubbed;
+    Object.defineProperty(mainContainer, 'offsetHeight', { configurable: true, value: mockHeight });
+
+    const expectedFillHeight = advanceTimeinMs / timeinMs * mockHeight;
+    await waitFor(() => expect(progressBar).toHaveStyleRule('height', `${expectedFillHeight}px`));
+    await waitFor(() => expect(progressBar).toHaveStyleRule('bottom', '0'));
+  });
+
+  it('should render with a flow direction from topToBottom', async () => {
+    const { container } = render(<ReactTimerBar time={timeinMs / 1000} fillColor="blue" direction='topToBottom'/>);
+    act(() => {
+      jest.advanceTimersByTime(advanceTimeinMs);
+    });
+    const mainContainer: HTMLDivElement = container.querySelector('#container');
+    const progressBar: HTMLDivElement = container.querySelector('#progress-bar');
+
+    // jsdom cannot support layouts so measurements will always return 0 unless stubbed;
+    Object.defineProperty(mainContainer, 'offsetHeight', { configurable: true, value: mockHeight });
+
+    const expectedFillHeight = advanceTimeinMs / timeinMs * mockHeight;
+    await waitFor(() => expect(progressBar).toHaveStyleRule('height', `${expectedFillHeight}px`));
+    await waitFor(() => expect(progressBar).toHaveStyleRule('top', '0'));
+  });
 });
