ca9be6d502108d7bc18b26fbd314ef2af3641dac
Eddie Thuo <thuoeddie17@gmail.com>
2022-04-10 16:05:44 +0100

    doc: update demo time & direction

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { ReactTimerBar } from './ReactTimerBar';
 
-ReactDOM.render(<ReactTimerBar fillColor="blue" time={5} direction="leftToRight" />, document.getElementById('output'));
+ReactDOM.render(<ReactTimerBar fillColor="blue" time={20} direction="topToBottom" />, document.getElementById('output'));
