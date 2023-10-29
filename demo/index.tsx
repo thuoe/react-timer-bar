67d26c1717fd5f1860675faa581280f1142f2732
Eddie Thuo <thuoeddie17@gmail.com>
2023-10-29 21:34:52 +0000

    doc: snippets for installation, get started & directions

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './font.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { CodeBlock } from 'react-code-blocks';
 import { ReactTimerBar, ReactTimerProps } from '../src';
 import styled from '@emotion/styled';
 
 
-const FlexDirectionDemo = ({ direction, ...rest }: ReactTimerProps) => {
+const FlexDirectionDemo = ({ direction, snippet, ...rest }: ReactTimerProps & { snippet: string }) => {
   const heading = direction === 'bottomToTop' ? 'Bottom to top'
     : direction === 'topToBottom' ? 'Top to Bottom' 
       : direction === 'leftToRight' ? 'Left to Right' 
@@ -15,6 +17,7 @@ const FlexDirectionDemo = ({ direction, ...rest }: ReactTimerProps) => {
     <div style={{ margin: '0 auto' }}>
       <h2>{heading}</h2>
       <ReactTimerBar {...rest} direction={direction}/>
+      <CodeBlock language="typescript" text={snippet} showLineNumbers={false} />
     </div>
   );
 };
@@ -31,17 +34,42 @@ const DemoPage = () => {
     time: 20,
     fillColor: 'blue',
   };
+  
   return (
     <>
     <Card>
     <h2>Installation</h2>
 
+    <p>NPM:</p>
+    <CodeBlock text='npm install @thuoe/react-timer-bar' showLineNumbers={false} wrapLongLines />
+
+    <p>Yarn:</p>
+    <CodeBlock text='yarn add @thuoe/react-timer-bar' showLineNumbers={false} wrapLongLines />
+
+
+    <h2>Get started</h2>
+    <CodeBlock 
+      wrapLongLines
+      language='typescript'
+      text={
+        `
+        import { ReactTimerBar } from '@thuoe/react-timer-bar'
+        const MyComponent = () => { 
+          return (
+            <ReactTimerBar time={20} fillColor="red" direction="leftToRight" />
+          )
+        }
+        export default MyComponent
+        `
+      } 
+      showLineNumbers={false} />
+
     <h2>Directions</h2>
     <p>These are various directions you can use...</p>
-      <FlexDirectionDemo direction='bottomToTop' {...props} />
-      <FlexDirectionDemo direction='topToBottom' {...props}/>
-      <FlexDirectionDemo direction='leftToRight' {...props} />
-      <FlexDirectionDemo direction='rightToLeft' {...props}/>
+      <FlexDirectionDemo snippet='<ReactTimerBar time={20} fillColor="red" direction="bottomToTop" />' direction='bottomToTop' {...props} />
+      <FlexDirectionDemo snippet='<ReactTimerBar time={20} fillColor="red" direction="topToBottom" />' direction='topToBottom' {...props}/>
+      <FlexDirectionDemo snippet='<ReactTimerBar time={20} fillColor="red" direction="leftToRight" />' direction='leftToRight' {...props} />
+      <FlexDirectionDemo snippet='<ReactTimerBar time={20} fillColor="red" direction="rightToLeft" />' direction='rightToLeft' {...props}/>
     </Card>
     </>
   );
